81dd4c5fef46eb635fbb355a002bca2559a7fea4
Max Bolsunovsky <jamax.cherry@gmail.com>
2025-09-17 01:11:40 +0300

    Initial commit

---
+++ src/pages/news-page/NewsPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface NewPage {
+	email: string;
+}
+
+function NewsPage() {
+	return (
+		<div>NewsPage</div>
+	)
+}
+
+export default NewsPage
---
+++ src/pages/pages.js
@@ -0,0 +1,5 @@
+import AuthPage from './auth-page';
+import NewsPage from './news-page';
+import MainPage from "./main-page";
+
+export { AuthPage, NewsPage, MainPage };
