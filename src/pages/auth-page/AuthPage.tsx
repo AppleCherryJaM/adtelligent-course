cc1259099bcecc3410d584d941b69b6ecf0f1a9b
Max Bolsunovsky <jamax.cherry@gmail.com>
2025-09-17 02:50:58 +0300

    Added basic forms for auth

--- src/pages/auth-page/AuthPage.tsx
+++ src/pages/auth-page/AuthPage.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { useState } from 'react'
+import LoginForm from './Login'
+import RegisterForm from './Register'
 
-function AuthPage() {
-	return (
-		<div>AuthPage</div>
-	)
+
+const AuthPage = ()  => {
+	const [isLogin, setIsLogin] = useState<boolean>(true)
+	const handleSwitchForm = () => {
+		setIsLogin(!isLogin)
+	}
+	return <>
+        {isLogin ? <LoginForm /> : <RegisterForm/>}
+		<button onClick={handleSwitchForm}>{isLogin ? "Doesn't have an account? Register": "Have an account? Login"}</button>
+	</>
+	
 }
 
 export default AuthPage
