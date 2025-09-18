b2cb6954ff9abd8bac001f6e5308f5cdda0432fc
Max Bolsunovsky <jamax.cherry@gmail.com>
2025-09-18 18:04:24 +0300

    Edited by Biome

--- src/types/NewsType.ts
+++ src/types/NewsType.ts
@@ -1,7 +1,7 @@
 export interface NewsType {
 	id: string;
 	title: string;
-	image: string | null;
-	mainText: string | null;
-	date: string | null;
+	image: string;
+	mainText: string;
+	date: string;
 }
